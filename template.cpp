0f7f49cfbc4b7e22ad4b58599f37d5f67c8ec931
SAIKAT BISHAL <54259560+saikatbishal@users.noreply.github.com>
2021-05-30 21:25:54 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -29,11 +29,6 @@ typedef vector<pii>		vpii;
 typedef vector<pl>		vpl;
 typedef vector<vi>		vvi;
 typedef vector<vl>		vvl;
-mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
-int rng(int lim) {
-	uniform_int_distribution<int> uid(0,lim-1);
-	return uid(rang);
-}
 int mpow(int base, int exp); 
 void ipgraph(int n, int m);
 void dfs(int u, int par);
@@ -51,7 +46,6 @@ void solve() {
 
 int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
 
     int t = 1;
     cin >> t;
